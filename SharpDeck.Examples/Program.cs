5b988a5ab9602e26f1f2ac5c8dc903057405fcfb
James Rozos <skrlx@outlook.com>
2017-12-21 18:04:48 +1100

    Added examples

---
+++ SharpDeck.Examples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SharpDeck.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SharpDeck deck = new SharpDeck();
+            Console.WriteLine("There are " + deck.Cards.Count + " cards in this deck.");
+
+            Player player1 = new Player(); // Player class for example only
+            Player player2 = new Player();
+            player1.hand = deck.Deal();
+            player2.hand = deck.Deal();
+
+            Console.WriteLine("Player 1 has the " + player1.hand.ToString() + ".");
+            Console.WriteLine("Player 2 has the " + player2.hand.ToString() + ".");
+            Console.Read();
+
+        }
+    }
+
+    public class Player // Player class for example only
+    {
+        public Card hand;
+    }
+}
--- SharpDeck.Examples/Properties/AssemblyInfo.cs
+++ SharpDeck.Examples/Properties/AssemblyInfo.cs
@@ -1,18 +1,26 @@
 ﻿using System.Reflection;
-using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpDeck")]
+[assembly: AssemblyTitle("SharpDeck.Examples")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SharpDeck")]
+[assembly: AssemblyProduct("SharpDeck.Examples")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("38e952bb-42de-420a-a7f1-f480b2a00838")]
 
 // Version information for an assembly consists of the following four values:
 //
